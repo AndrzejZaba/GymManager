e0c71d2e7518c1e89145c902c2d51f791417d5b4
AndrzejZaba <andrzejzaba@onet.pl>
2024-01-15 14:45:26 +0100

    Data Annotation validation

--- GymManager.Application/Contacts/Commands/SendContactEmail/SendContactEmailCommandValidator.cs
+++ GymManager.Application/Contacts/Commands/SendContactEmail/SendContactEmailCommandValidator.cs
@@ -5,9 +5,9 @@ namespace GymManager.Application.Contacts.Commands.SendContactEmail;
 public class SendContactEmailCommandValidator : 
     AbstractValidator<SendContactEmailCommand>
 {
-    public SendContactEmailCommandValidator()
-    {
-        RuleFor(x => x.Name)
-            .NotEmpty().WithMessage("Pole 'Imię i Nazwisko' jest wymagane.");
-    }
+    //public SendContactEmailCommandValidator()
+    //{
+    //    RuleFor(x => x.Name)
+    //        .NotEmpty().WithMessage("Pole 'Imię i Nazwisko' jest wymagane.");
+    //}
 }
