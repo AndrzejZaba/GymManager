9ebbdfcd1c199bf9ce1c33f231a8d9fe70e26aa5
AndrzejZaba <andrzejzaba@onet.pl>
2024-01-23 14:01:22 +0100

    Register View works

--- GymManager.UI/Areas/Identity/Data/ApplicationDbContext.cs
+++ GymManager.UI/Areas/Identity/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using GymManager.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace GymManager.Infrastructure.Persistence;
 
-public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
