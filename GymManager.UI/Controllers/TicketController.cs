b60b40e9539dfcbd8c952d888281d7283a86013f
AndrzejZaba <andrzejzaba@onet.pl>
2024-01-29 21:12:29 +0100

    TicketController added

---
+++ GymManager.UI/Controllers/TicketController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymManager.UI.Controllers
+{
+    [Authorize]
+    public class TicketController : BaseController
+    {
+        public IActionResult Tickets()
+        {
+            return View();
+        }
+    }
+}
