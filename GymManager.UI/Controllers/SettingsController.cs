8c6e0e85fa23ca96694f1e337c01033bf65a14c5
AndrzejZaba <andrzejzaba@onet.pl>
2024-02-24 20:07:16 +0100

    Settings tabs added

---
+++ GymManager.UI/Controllers/SettingsController.cs
@@ -0,0 +1,15 @@
+﻿using GymManager.Application.Dictionaries;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace GymManager.UI.Controllers;
+
+[Authorize(Roles = RolesDict.Administrator)]
+public class SettingsController : BaseController
+{
+    public IActionResult Settings()
+    {
+        return View();
+    }
+}
