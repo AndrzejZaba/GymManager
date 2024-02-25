33febbad433de3a6e3dbe80c97e2dec4994f7310
AndrzejZaba <andrzejzaba@onet.pl>
2024-02-25 18:58:18 +0100

    Edit settings added

--- GymManager.UI/Controllers/SettingsController.cs
+++ GymManager.UI/Controllers/SettingsController.cs
@@ -1,4 +1,5 @@
 ﻿using GymManager.Application.Dictionaries;
+using GymManager.Application.Settings.Commands.EditSettings;
 using GymManager.Application.Settings.Queries.GetSettings;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -12,5 +13,19 @@ public class SettingsController : BaseController
     public async Task<IActionResult> Settings()
     {
         return View(await Mediator.Send(new GetSettingsQuery()));
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> EditSettings(IList<SettingsDto> model)
+    {
+        var command = new EditSettingsCommand();
+        model.ToList().ForEach(x => command.Positions.AddRange(x.Positions));
+
+        await Mediator.Send(command);
+
+        TempData["Success"] = "Ustawienia zostały zaktualizowane.";
+
+        return RedirectToAction("Settings");
     }
 }
